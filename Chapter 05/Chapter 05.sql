3a6dceac068ae431fc6422dce0e56d2b093ef577
TashChetty <natasha.chetty2@gmail.com>
2023-11-21 16:17:26 +0200

    Chapter 5

--- Chapter 05/Chapter 05.sql
+++ Chapter 05/Chapter 05.sql
@@ -198,6 +198,16 @@ SELECT sum(p0010001) AS "County Sum",
        WITHIN GROUP (ORDER BY P0010001) AS "50th Percentile"
 FROM us_counties_2010;
 
+--corrected 5.15
+SELECT 
+    SUM(p0010001) AS "County Sum",
+    ROUND(AVG(p0010001), 0) AS "County Average",
+    PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY p0010001) AS "County Median",
+    PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY p0010001) AS "50th Percentile"
+FROM us_counties_2010;
+--
+
+
 -- Listing 5-16: Finding the most-frequent value with mode()
 
 SELECT mode() WITHIN GROUP (ORDER BY p0010001)
